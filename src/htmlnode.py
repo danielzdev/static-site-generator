c6776f09b6f42a0db85475b1c4b15fa57996606a
Daniel Zapata <zdaniel4771@gmail.com>
2025-07-31 14:42:40 -0700

    fixed props_to_html() in htmlnode.py, and relevant tests. added Leafnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,10 +10,25 @@ class HTMLNode:
 
     def props_to_html(self):
         props = ""
-        for value in self.props.values():
-            props = props + " " + value
+        for key, value in self.props.items():
+            props = props + " " + f'{key}="{value}"'
 
         return props
 
     def __repr__(self):
         return f"HTMLNode(tag = {self.tag}, value = {self.value}, children = {self.children}, props = {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, [], props=props or {})
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("ERROR: Node has no value")
+        elif self.tag is None:
+            return self.value
+        elif self.props:
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        else:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -9,19 +9,19 @@ class TestHTMLNodePropsToHTML(unittest.TestCase):
         self.assertEqual(node.props_to_html(), "")
 
     def test_one_prop(self):
-        node = HTMLNode(props={"href": 'href="/url"'})
-        self.assertEqual(node.props_to_html(), ' href="/url"')
+        node = HTMLNode("p", "This is an htmlnode", props={"href": "www.google.com"})
+        self.assertEqual(node.props_to_html(), ' href="www.google.com"')
 
     def test_multiple_props(self):
         node = HTMLNode(
             props={
-                "href": 'href="/url"',
-                "target": 'target="_blank"',
-                "class": 'class="btn"',
+                "href": "www.google.com",
+                "target": "_blank",
+                "class": "btn",
             }
         )
         self.assertEqual(
-            node.props_to_html(), ' href="/url" target="_blank" class="btn"'
+            node.props_to_html(), ' href="www.google.com" target="_blank" class="btn"'
         )
 
     def test_empty_props_dict(self):
@@ -31,9 +31,9 @@ class TestHTMLNodePropsToHTML(unittest.TestCase):
     def test_props_with_children(self):
         node = HTMLNode(
             children=[HTMLNode()],
-            props={"id": 'id="main"', "style": 'style="color:red;"'},
+            props={"id": "main", "style": "color: red;"},
         )
-        self.assertEqual(node.props_to_html(), ' id="main" style="color:red;"')
+        self.assertEqual(node.props_to_html(), ' id="main" style="color: red;"')
 
 
 if __name__ == "__main__":
