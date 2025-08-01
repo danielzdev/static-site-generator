8543d070bf43fd076fcf6b206dd3734652731a77
Daniel Zapata <zdaniel4771@gmail.com>
2025-08-01 00:28:46 -0700

    added split_nodes_delimiter() and tests

---
+++ src/util.py
@@ -0,0 +1,25 @@
+from src.textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    delimiters = {"*": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+
+    if delimiter not in delimiters:
+        raise ValueError(f"Invalid delimiter: {delimiter}")
+
+    for node in old_nodes:
+        if delimiter not in node.text:
+            continue
+        index = node.text.index(delimiter)
+        second_index = node.text.find(delimiter, index + 1)
+
+        new_nodes.extend(
+            [
+                TextNode(node.text[:index], TextType.TEXT),
+                TextNode(node.text[index : second_index + 1], text_type),
+                TextNode(node.text[second_index + 1 :], TextType.TEXT),
+            ]
+        )
+
+    return new_nodes
---
+++ tests/test_splitnodes.py
@@ -0,0 +1,58 @@
+import unittest
+
+from src.textnode import TextNode, TextType
+from src.util import split_nodes_delimiter
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_valid_split_code(self):
+        nodes = [TextNode("Here is `code`", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result[0].text, "Here is ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "`code`")
+        self.assertEqual(result[1].text_type, TextType.CODE)
+        self.assertEqual(result[2].text, "")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+    def test_no_delimiter_present(self):
+        nodes = [TextNode("No formatting here", TextType.TEXT)]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(result, [])
+
+    def test_invalid_delimiter(self):
+        nodes = [TextNode("Some text with `code`", TextType.TEXT)]
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter(nodes, "~", TextType.CODE)
+
+    def test_multiple_textnodes_mixed_delimiters(self):
+        nodes = [
+            TextNode("No delimiter here", TextType.TEXT),
+            TextNode("Middle has *bold* text, neat.", TextType.TEXT),
+            TextNode("Another one with no match", TextType.TEXT),
+            TextNode("Some *MORE* bold text :D", TextType.TEXT),
+            TextNode("Middle has _italic_ text, neat.", TextType.ITALIC),
+        ]
+        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
+
+        self.assertEqual(len(result), 6)
+
+        self.assertEqual(result[0].text, "Middle has ")
+        self.assertEqual(result[0].text_type, TextType.TEXT)
+        self.assertEqual(result[1].text, "*bold*")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+
+        self.assertEqual(result[2].text, " text, neat.")
+        self.assertEqual(result[2].text_type, TextType.TEXT)
+
+        self.assertEqual(result[3].text, "Some ")
+        self.assertEqual(result[3].text_type, TextType.TEXT)
+        self.assertEqual(result[4].text, "*MORE*")
+        self.assertEqual(result[4].text_type, TextType.BOLD)
+
+        self.assertEqual(result[5].text, " bold text :D")
+        self.assertEqual(result[5].text_type, TextType.TEXT)
+
+
+if __name__ == "__main__":
+    unittest.main()
