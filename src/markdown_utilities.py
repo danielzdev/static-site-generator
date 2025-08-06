13db163298455589637c89dce4ee142e68e2711c
Daniel Zapata <zdaniel4771@gmail.com>
2025-08-06 16:10:54 -0700

    added a block to html node function

--- src/markdown_utilities.py
+++ src/markdown_utilities.py
@@ -1,3 +1,90 @@
+from src.htmlnode import LeafNode, ParentNode
+from src.markdown_block import BlockType, get_block_type
+from src.textnode import text_node_to_html_node
+from src.util import text_to_textnodes
+
+
 def markdown_to_blocks(markdown):
     return [block.strip() for block in markdown.split("\n\n") if block.strip()]
 
+
+def markdown_to_html_node(markdown):
+    parent_div = ParentNode("div", children=[])
+    children = parent_div.children
+
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        node = convert_block_to_node(block)
+        children.append(node)
+
+    return parent_div
+
+
+def convert_block_to_node(block):
+    type = get_block_type(block)
+
+    if type == BlockType.CODE:
+        return convert_code_block_to_node(block)
+    elif type == BlockType.PARAGRAPH:
+        return convert_paragraph_block_to_node(block)
+    elif type == BlockType.HEADING:
+        return convert_heading_block_to_node(block)
+    else:
+        return convert_special_block_to_node(block)
+
+
+def convert_code_block_to_node(block):
+    lines = block.splitlines(keepends=True)
+    cleaned = lines[1:-1]
+    only_code = "".join(cleaned)
+    return ParentNode("pre", children=[LeafNode("code", only_code)])
+
+
+def convert_paragraph_block_to_node(block):
+    block = block.replace("\n", " ")
+
+    leaf_nodes = []
+    text_nodes = text_to_textnodes(block)
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        leaf_nodes.append(html_node)
+
+    return ParentNode("p", children=leaf_nodes)
+
+
+def convert_heading_block_to_node(block):
+    headings = {1: "h1", 2: "h2", 3: "h3", 4: "h4", 5: "h5", 6: "h6"}
+
+    count = 0
+    for char in block:
+        if char == "#":
+            count += 1
+        else:
+            break
+
+    tag = headings[count]
+    text = block[count + 1 :]
+    child_nodes = text_to_textnodes(text)
+    return ParentNode(tag, children=child_nodes)
+
+
+def convert_special_block_to_node(block):
+    parent_tags = {">": "blockquote", "-": "ul", "1": "ol"}
+    tag = parent_tags[block[0]]
+    parent = ParentNode(tag, children=[])
+
+    lines = block.splitlines()
+    for line in lines:
+        line = line[1:]
+        text_nodes = text_to_textnodes(line)
+
+        leaf_nodes = []
+        for text_node in text_nodes:
+            html_node = text_node_to_html_node(text_node)
+            leaf_nodes.append(html_node)
+
+        child_tag = "p" if tag == "blockquote" else "li"
+        parent_node = ParentNode(child_tag, children=leaf_nodes)
+        parent.children.append(parent_node)
+
+    return parent
---
+++ tests/test_markdown_to_html_node.py
@@ -0,0 +1,89 @@
+import unittest
+
+from src.markdown_utilities import markdown_to_html_node
+
+
+class TestMarkdownToHtmlNode(unittest.TestCase):
+    def test_one_sentence(self):
+        md = "This is a sentence."
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><p>This is a sentence.</p></div>")
+
+    def test_one_setence_with_some_markdown(self):
+        md = "This is a sentence with **bold** and _italic_ text."
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><p>This is a sentence with <b>bold</b> and <i>italic</i> text.</p></div>")
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+        pass
+
+    def test_quote_block(self):
+        md = """
+>This is a quote block
+>This is **another** quote block
+>This is a third quote block with an _italic_ word
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote><p>This is a quote block</p><p>This is <b>another</b> quote block</p><p>This is a third quote block with an <i>italic</i> word</p></blockquote></div>",
+        )
+
+    def test_multiple_blocks(self):
+        md = """
+>This is a quote block
+>This is **another** quote block
+>This is a third quote block with an _italic_ word
+
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote><p>This is a quote block</p><p>This is <b>another</b> quote block</p><p>This is a third quote block with an <i>italic</i> word</p></blockquote><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
